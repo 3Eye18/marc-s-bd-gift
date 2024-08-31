a8be5cf72f1d0c2b7b8c8b77af622eafe6be37bf
Nguyễn Anh Vũ <101244686+3Eye18@users.noreply.github.com>
2024-08-31 11:57:02 +0700

    update clover, added a room

--- clover.gd
+++ clover.gd
@@ -1,38 +1,26 @@
 extends CharacterBody2D
 
 
-@export var SPEED: float = 100
+@export var SPEED: float = 300
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	var speed: float = SPEED
-	if Input.is_action_pressed("sprint"):
-		speed = SPEED * 2
-	
 	velocity = direction * speed
 	
-	if velocity == Vector2.ZERO: 
-			$AnimatedSprite2D.stop()
-	if Input.is_action_pressed("sprint"):
-		if velocity.x > 0 && velocity.y == 0:
-			$AnimatedSprite2D.play("right_sprint", 1.5)
-		elif velocity.x < 0 && velocity.y == 0:
-			$AnimatedSprite2D.play("left_sprint", 1.5)
-		elif velocity.x == 0 && velocity.y > 0:
-			$AnimatedSprite2D.play("down_sprint", 1.5)
-		elif velocity.x == 0 && velocity.y < 0:
-			$AnimatedSprite2D.play("up_sprint", 1.5)
-	else:
-		if velocity.x > 0 && velocity.y == 0:
-			$AnimatedSprite2D.play("right_walk")
-		elif velocity.x < 0 && velocity.y == 0:
-			$AnimatedSprite2D.play("left_walk")
-		elif velocity.x == 0 && velocity.y > 0:
-			$AnimatedSprite2D.play("down_walk")
-		elif velocity.x == 0 && velocity.y < 0:
-			$AnimatedSprite2D.play("up_walk")
+	if direction.x > 0 && direction.y == 0:
+		$AnimatedSprite2D.play("right_walk")
+	elif direction.x < 0 && direction.y == 0:
+		$AnimatedSprite2D.play("left_walk")
+	elif direction.x == 0 && direction.y < 0:
+		$AnimatedSprite2D.play("up_walk")
+	elif direction.x == 0 && direction.y > 0:
+		$AnimatedSprite2D.play("down_walk")
+	
+	if direction == Vector2.ZERO: 
+		$AnimatedSprite2D.stop()
 	
 	move_and_slide()
 	
