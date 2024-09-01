94b46c68305d34dd6a8577606265170badc8411a
Nguyễn Anh Vũ <101244686+3Eye18@users.noreply.github.com>
2024-09-01 19:55:33 +0700

    ultimate sprite update :xd:

--- Script/clover.gd
+++ Script/clover.gd
@@ -23,22 +23,3 @@ func _physics_process(_delta):
 		$AnimatedSprite2D.stop()
 	
 	move_and_slide()
-	
-	if get_slide_collision_count():
-		for i in get_slide_collision_count():
-			var collider = get_slide_collision(i).get_collider()
-			
-			var player_collision_box = $CollisionShape2D
-			var obstacle_collision_box = collider.get_node("CollisionShape2D")
-			
-			## Calculate positions
-			#print("Player: ", player_collision_box.global_position.y, " Rock: ",obstacle_collision_box.global_position.y)
-			var player_bottom = player_collision_box.global_position.y + player_collision_box.shape.size.y / 2
-			var obstacle_top = obstacle_collision_box.global_position.y - obstacle_collision_box.shape.size.y * 4 /2
-			#print("Bot: ", player_bottom, " Top: ",obstacle_top)
-			
-			## Adjust Z-index
-			if player_bottom < obstacle_top:
-				collider.z_index = 1
-			else:
-				collider.z_index = 0
--- Script/honeydew_resort_inside.gd
+++ Script/honeydew_resort_inside.gd
@@ -1,10 +1,28 @@
 extends Node
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	sprite_handling()
+
+
+func sprite_handling():
+	var player_collision_box = $Clover/CollisionShape2D
+	var player_bottom = player_collision_box.global_position.y + player_collision_box.shape.size.y / 2
+	
+	var child_count = $House/Furnitures.get_child_count()
+	for i in child_count:
+		var child = $House/Furnitures.get_child(i)
+		if is_instance_of(child, StaticBody2D):
+			var child_collision_box = child.get_node("CollisionShape2D")
+			var child_top = child_collision_box.global_position.y - child_collision_box.shape.size.y * 4 /2
+			
+			if player_bottom < child_top:
+				child.z_index = 1
+			else:
+				child.z_index = 0
