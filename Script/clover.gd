f5182d711be5cd3334771d2d07beb25ae800a56b
Nguyễn Anh Vũ <101244686+3Eye18@users.noreply.github.com>
2024-09-05 17:01:08 +0700

    camera clamp

--- Script/clover.gd
+++ Script/clover.gd
@@ -1,12 +1,11 @@
 extends CharacterBody2D
 
 
-@export var SPEED: float = 15000
+@export var SPEED: float = 30000
 
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	
 	
 	if direction == Vector2.ZERO:
 		$AnimationTree.get("parameters/playback").travel("Idle")
@@ -14,8 +13,9 @@ func _physics_process(delta):
 		$AnimationTree.get("parameters/playback").travel("Walk")
 		$AnimationTree.set("parameters/Idle/blend_position", direction)
 		$AnimationTree.set("parameters/Walk/blend_position", direction)
-	
-	
+		
+	if get_slide_collision(0):
+		$AnimationTree.get("parameters/playback").travel("Idle")
 	
 	var speed: float = SPEED
 	velocity = direction * speed * delta
